450c6dc376644c6a1af338f0a44857c88a94c04d
Simeao jose <simeaojs@gmail.com>
2023-05-16 11:37:49 -0300

    Update velocidade.py

--- Logica de Programação/Exercicio curso em video/velocidade.py
+++ Logica de Programação/Exercicio curso em video/velocidade.py
@@ -1,10 +1,23 @@
 
-velocidade=float(input(' velocidade KM/h:'))
-limite=80
-multa=7
-if velocidade>limite:
-    acima_do_limite=velocidade-limite
-    multa=acima_do_limite*multa
-    print(' multa por excesso',multa)
+'''velocidade = float(input(' velocidade KM/h:'))
+limite = 80
+multa = 7
+if velocidade > limite:
+    acima_do_limite = velocidade - limite
+    multa = acima_do_limite * multa
+    print(f' multa por excesso de velocidade: R${multa}')
 else:
-    print(' Velocidade normal')
+    print(' Velocidade normal')'''
+
+velocidade = float(input(' Velocidade KM/H:'))
+limite = 80 
+multa = 10 
+if velocidade > limite:
+
+    acima_do_limite  = velocidade - limite
+    multa = acima_do_limite * multa
+
+    print(f' Multa por excesso de velocidade: R${multa}')
+
+else:
+    print('Não ultrapassou o limite de velocidade')    
