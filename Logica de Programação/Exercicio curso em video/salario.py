cd9c32d1d9f39dfa86a99bdb5022c4e0916f1db4
Simeao jose <simeaojs@gmail.com>
2023-05-16 11:48:53 -0300

    Refatoração Salario.py

--- Logica de Programação/Exercicio curso em video/salario.py
+++ Logica de Programação/Exercicio curso em video/salario.py
@@ -1,5 +1,5 @@
 
-salario_atual=float(input(' Digite seu salario atual:'))
+'''salario_atual=float(input(' Digite seu salario atual:'))
 if salario_atual > 1250:
     novo_salario=salario_atual+(salario_atual*0.10)
     
@@ -7,4 +7,14 @@ if salario_atual > 1250:
 if salario_atual<=1249:
     novo_salario=salario_atual+(salario_atual*0.15)
 
-print(' Salario novo',novo_salario)
+print(' Salario novo',novo_salario)'''
+
+salario = float(input(' Digite seu salario atual:'))
+
+if salario > 1250:
+    novo_salario = salario + (salario * 0.10)
+
+if salario <= 1249:
+    novo_salario = salario + (salario * 0.15)
+
+print(f' Salario novo: R${novo_salario}')    
