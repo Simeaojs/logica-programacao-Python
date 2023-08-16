1d62b48d51299729a952b59fd4b864babc5d208c
Simeao jose <simeaojs@gmail.com>
2023-08-16 19:31:16 -0300

    update

--- Logica de Programação/Exercicios Sabado  01 de Abril/Exerc 19 (soma numeros inteiros).py
+++ Logica de Programação/Exercicios Sabado  01 de Abril/Exerc 19 (soma numeros inteiros).py
@@ -1,11 +1,3 @@
-# Loop FOR
-
-'''print()
-numero = int(input('Digite um numero: '))
-soma = 0
-for i in range(1, numero+1):
-    soma += i
-print('A soma do numeros de 1 ate:',numero,'e:',soma)'''
 
 # Loop While
 
@@ -16,3 +8,5 @@ while i<=numero:
     soma+=i
     i+=1
 print('A soma do numeros de 1 ate:',numero,'e',soma)
+
+
